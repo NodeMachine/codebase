42d3a69d08ec2adf875d8ea110baaffbbc4a46ae
Sofia Benitez <sofia.benitez39@gmail.com>
2019-06-28 17:57:20 -0400

    feat: added problem database helper functions

--- server/db/queryFunctions/userQueryFunctions.js
+++ server/db/queryFunctions/userQueryFunctions.js
@@ -1,18 +1,4 @@
-const firebase = require('firebase')
-const keys = require('../../keys')
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: keys.apiKey,
-  authDomain: keys.authDomain,
-  databaseURL: keys.databaseURL,
-  projectId: keys.projectId,
-  storageBucket: keys.storageBucket,
-  messagingSenderId: keys.messagingSenderId,
-  appId: keys.appId
-}
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firestore()
+const db = require('./index')
 
 const createUser = async obj => {
   try {
