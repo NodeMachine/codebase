0ad4a0e85a462c0baa99bbcb9bb39fd98a8f9f97
Sofia Benitez <sofia.benitez39@gmail.com>
2019-06-29 17:01:36 -0400

    fix: removed components, added classes

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,44 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import 'materialize-css'
-import M from 'materialize-css'
+import {logout} from '../store'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 
-class Navbar extends React.Component {
-  constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
-    this.isLoggedIn = this.isLoggedIn.bind(this)
-  }
-  componentDidMount() {
-    M.Sidenav.init(this.Navbar)
-  }
-  render() {
-    return (
-      <div>
-        <h1>Our Codewars Clone</h1>
-        <Navbar brand={<a />} alignLinks="right">
-          {this.isLoggedIn ? (
-            <div className="right">
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={this.handleClick}>
-                Logout
-              </a>
-            </div>
-          ) : (
-            <div className="right">
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-          )}
-        </Navbar>
-        <hr />
-      </div>
-    )
-  }
-}
+const Navbar = ({handleClick, isLoggedIn}) => (
+  <div>
+    <h1>Our Codewars Clone</h1>
+    <nav className="nav-wraper">
+      {isLoggedIn ? (
+        <div className="right">
+          {/* The navbar will show these links after you log in */}
+          <Link to="/home">Home</Link>
+          <a href="#" onClick={handleClick}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        <div className="right">
+          {/* The navbar will show these links before you log in */}
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      )}
+    </nav>
+    <hr />
+  </div>
+)
 
 /**
  * CONTAINER
