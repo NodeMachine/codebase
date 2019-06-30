f5c2cec26da74fbdf820f41c44945611b49af4b7
Sofia Benitez <sofia.benitez39@gmail.com>
2019-06-30 03:12:05 -0400

    feat: added user problem list to user profile page

---
+++ client/components/userProblemsList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const styles = {
+  icon: {marginTop: 30}
+}
+
+const UserProblemList = props => {
+  return (
+    <ul>
+      {props.userProblems.map(el => {
+        return (
+          <li key={el.id}>
+            <div className="row">
+              <div className="col s8">
+                <h3>{el.title}</h3>
+                <h5>{el.category}</h5>
+                <p>Difficulty: {el.points < 100 ? 'Easy' : 'Medium'}</p>
+              </div>
+              <div className="col s4 " style={styles.icon}>
+                {el.isSolved ? (
+                  <i className="medium material-icons green-text">
+                    check_circle
+                  </i>
+                ) : (
+                  <i className="medium material-icons orange-text ">
+                    check_circle
+                  </i>
+                )}
+              </div>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default UserProblemList
--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -1,31 +1,59 @@
 import React, {Component} from 'react'
-
-// Until we have a CSS file
-const styles = {
-  userInfo: {display: 'flex'}
-}
+import UserProblemsList from './userProblemsList'
 
 class UserProfile extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      id: 1,
-      firstName: 'John',
-      lastName: 'Smith',
-      location: 'New York',
-      bio: 'Likes to set up Docker containers for fun',
-      interests: 'Docker'
+      user: {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Smith',
+        location: 'New York',
+        bio: 'Likes to set up Docker containers for fun',
+        interests: 'Docker',
+        photo: 'https://i.ebayimg.com/images/g/eYkAAOSwALtaWWmr/s-l300.jpg'
+      },
+      userProblems: [
+        {
+          id: 1,
+          title: 'Sum two digits',
+          points: 80,
+          category: 'arrays',
+          isSolved: 0
+        },
+        {
+          id: 2,
+          title: 'Return a string',
+          points: 1,
+          category: 'strings',
+          isSolved: 1
+        }
+      ]
     }
   }
   render() {
     return (
-      <div style={styles.userInfo}>
-        <h1>
-          {this.state.firstName} {this.state.lastName}
-        </h1>
-        <h2>"City:"{this.state.location}</h2>
-        <p>"Bio:"{this.state.bio}</p>
-        <p>"Interests:"{this.state.interests}</p>
+      <div>
+        <div className="row">
+          <div className="card">
+            <div className="col s3 card-image">
+              <img src={this.state.user.photo} />
+            </div>
+            <div className="col s3 card-content">
+              <h1>
+                {this.state.user.firstName} {this.state.user.lastName}
+              </h1>
+              <h4>{this.state.user.location}</h4>
+              <p>{this.state.user.bio}</p>
+              <p>Interests:{this.state.user.interests}</p>
+            </div>
+          </div>
+          <div className="col s6 card-content">
+            <h2>Past Problems:</h2>
+            <UserProblemsList userProblems={this.state.userProblems} />
+          </div>
+        </div>
       </div>
     )
   }
