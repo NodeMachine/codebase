a7fe4eb9489d59658db531fc2cbc159ceca72551
Mercedes <mercedesgmiller92@gmail.com>
2019-07-01 16:35:58 -0400

    feat: created user redux store

--- client/store/user.js
+++ client/store/user.js
@@ -6,17 +6,22 @@ import history from '../history'
  */
 const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
+const GET_USERS = 'GET_USERS'
 
 /**
  * INITIAL STATE
  */
-const defaultUser = {}
+const initialState = {
+  allUsers: [],
+  singleUser: {}
+}
 
 /**
  * ACTION CREATORS
  */
-const getUser = user => ({type: GET_USER, user})
+const getUser = singleUser => ({type: GET_USER, singleUser})
 const removeUser = () => ({type: REMOVE_USER})
+const gotAllUsers = allUsers => ({type: GET_USERS, allUsers})
 
 /**
  * THUNK CREATORS
@@ -24,47 +29,89 @@ const removeUser = () => ({type: REMOVE_USER})
 export const me = () => async dispatch => {
   try {
     const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || defaultUser))
+    dispatch(getUser(res.data))
   } catch (err) {
     console.error(err)
   }
 }
 
-export const auth = (email, password, method) => async dispatch => {
-  let res
+export const login = (email, password) => async dispatch => {
   try {
-    res = await axios.post(`/auth/${method}`, {email, password})
-  } catch (authError) {
-    return dispatch(getUser({error: authError}))
-  }
-
-  try {
+    const res = await axios.put('/api/users/login', {email, password})
     dispatch(getUser(res.data))
-    history.push('/home')
-  } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const signup = (
+  firstName,
+  lastName,
+  email,
+  password
+) => async dispatch => {
+  try {
+    const res = await axios.post('/api/users/signup', {
+      firstName,
+      lastName,
+      email,
+      password
+    })
+    dispatch(getUser(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateProfile = (id, obj) => async dispatch => {
+  try {
+    await axios.post(`/api/users/update/${id}`, {update: obj})
+    const res = axios.get(`/api/users/${id}`)
+    dispatch(getUser(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getAllUsers = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/users')
+    dispatch(gotAllUsers(res.data))
+  } catch (error) {
+    console.log(error)
   }
 }
 
 export const logout = () => async dispatch => {
   try {
-    await axios.post('/auth/logout')
+    await axios.post('/api/users/logout')
     dispatch(removeUser())
-    history.push('/login')
+    dispatch(getAllUsers())
   } catch (err) {
     console.error(err)
+  }
+}
+
+export const deleteAccount = id => async dispatch => {
+  try {
+    await axios.delete(`/api/users/${id}`)
+    dispatch(removeUser())
+  } catch (error) {
+    console.log(error)
   }
 }
 
 /**
  * REDUCER
  */
-export default function(state = defaultUser, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return {...state, singleUser: action.user}
     case REMOVE_USER:
-      return defaultUser
+      return {...state, singleUser: {}}
+    case GET_USERS:
+      return {...state, allUsers: action.allUsers}
     default:
       return state
   }
