3924564d754c268a2c07781ee97eb768f8df7c3b
Sofia Benitez <sofia.benitez39@gmail.com>
2019-07-01 08:43:03 -0400

    style: added score to profile

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,10 +6,10 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <nav className="nav-wrapper">
-      <a className="brand-logo">Our Codewars Clone</a>
+    <nav>
+      <a>Our Codewars Clone</a>
       {isLoggedIn ? (
-        <div className="right  ">
+        <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
@@ -17,7 +17,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           </a>
         </div>
       ) : (
-        <div className="right ">
+        <div>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
