6bfb4587976219a293a9d673a44234dbe1a89efd
sam-peach <sampeachart@gmail.com>
2019-07-02 15:51:56 -0400

    chore: looking into getting user problems on login

--- client/components/singleProblem.js
+++ client/components/singleProblem.js
@@ -4,6 +4,7 @@ import ProblemDescription from './problemDescription'
 import axios from 'axios'
 import {connect} from 'react-redux'
 import {getSingleProblem} from '../store/problems'
+import {saveSolution} from '../store/user'
 import ResultWindow from './resultWindow'
 
 class SingleProblem extends Component {
@@ -24,9 +25,22 @@ class SingleProblem extends Component {
 
   componentDidUpdate() {
     if (!this.state.code) {
-      this.setState({
-        code: this.props.problem[0].defaultCode || 'NO CODE DEFINED'
-      })
+      if (this.props.user && this.props.user.problems) {
+        for (let i = 0; i < this.props.user.problems.length; i++) {
+          if (
+            this.props.user.problems[i].id === this.this.props.problem[0].id
+          ) {
+            this.setState({
+              code: this.props.user.problems[i].solution || 'NO CODE DEFINED'
+            })
+            break
+          }
+        }
+      } else {
+        this.setState({
+          code: this.props.problem[0].defaultCode || 'NO CODE DEFINED'
+        })
+      }
     }
   }
 
@@ -34,13 +48,28 @@ class SingleProblem extends Component {
     this.setState({code: newValue})
   }
 
-  handleSubmit() {
-    axios
-      .post(`/api/solution/${this.props.problem[0].id}`, this.state)
-      .then(returnResult => {
-        this.setState({result: returnResult.data})
-      })
-      .catch(err => console.log(err))
+  async handleSubmit() {
+    try {
+      const {data} = await axios.post(
+        `/api/solution/${this.props.problem[0].id}`,
+        this.state
+      )
+      if (this.props.user) {
+        const problem = this.props.problem[0]
+        const userId = this.props.user.id
+        const isSolved = data.every(val => val === 'true')
+        const solution = this.state.code
+        this.props.saveSolution(problem, userId, isSolved, solution)
+      }
+    } catch (error) {
+      console.error("Something went wrong submitting user's code", error)
+    }
+    // await axios
+    //   .post(`/api/solution/${this.props.problem[0].id}`, this.state)
+    //   .then(returnResult => {
+    //     this.setState({result: returnResult.data})
+    //   })
+    //   .catch(err => console.log(err))
   }
 
   handleReset() {
@@ -79,13 +108,16 @@ const style = {
 
 const mapState = state => {
   return {
-    problem: state.problems
+    problem: state.problems,
+    user: state.user.singleUser
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-    getSingleProblem: problemId => dispatch(getSingleProblem(problemId))
+    getSingleProblem: problemId => dispatch(getSingleProblem(problemId)),
+    saveSolution: (problem, userId, isSolved, solution) =>
+      dispatch(saveSolution(problem, userId, isSolved, solution))
   }
 }
 
