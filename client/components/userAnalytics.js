0466fba8bb610db2933b1d9f47486308743d77f6
Sofia Benitez <sofia.benitez39@gmail.com>
2019-07-06 14:29:45 -0400

    feat: hooked up category data to chart

--- client/components/userAnalytics.js
+++ client/components/userAnalytics.js
@@ -2,15 +2,6 @@ import React, {Component} from 'react'
 import {BarChart, Bar, Tooltip, Legend, PieChart, Pie} from 'recharts'
 import './userAnalytics.css'
 
-const data01 = [
-  {name: 'Group A', value: 400},
-  {name: 'Group B', value: 300},
-  {name: 'Group C', value: 300},
-  {name: 'Group D', value: 200},
-  {name: 'Group E', value: 278},
-  {name: 'Group F', value: 189}
-]
-
 export const UserAnalytics = props => {
   const problems = props.problems
   const easy = problems.filter(problem => problem.points < 20).length
@@ -19,11 +10,13 @@ export const UserAnalytics = props => {
   ).length
   const hard = problems.filter(problem => problem.points > 59).length
 
-  const categories = problems.map(problem => problem.category)
+  const categoriesData = []
+  problems.forEach(problem => {
+    const dataPoint = {name: problem.category, value: problem.points}
+    categoriesData.push(dataPoint)
+  })
 
-  console.log('CATEGORIES', categories)
-
-  const data = [
+  const difficultyData = [
     {
       name: 'Difficulty',
       easy: easy,
@@ -34,12 +27,12 @@ export const UserAnalytics = props => {
 
   return (
     <div className="charts-container">
-      <div>
-        <p>Difficulty breakdown:</p>
+      <div className="chart-item">
+        <p>Difficulty Breakdown</p>
         <BarChart
           width={200}
           height={300}
-          data={data}
+          data={difficultyData}
           margin={{
             top: 20,
             right: 30,
@@ -54,15 +47,15 @@ export const UserAnalytics = props => {
           <Bar dataKey="hard" stackId="a" fill="#0095A8" />
         </BarChart>
       </div>
-      <div>
-        <p>Category breakdown:</p>
-        <PieChart width={300} height={300}>
+      <div className="chart-item">
+        <p>Points by Category</p>
+        <PieChart width={300} height={300} className="pie-chart-container">
           <Pie
             dataKey="value"
             isAnimationActive={false}
-            data={data01}
-            cx={200}
-            cy={200}
+            data={categoriesData}
+            // cx={200}
+            // cy={200}
             outerRadius={80}
             fill="#26C6DA"
             label
